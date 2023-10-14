ff3e8166b2b55d3a2069e5d06a8bf18f78826f2a
GB-MyLang <ghanashyam.b@mylang.in>
2023-10-15 01:53:46 +0530

    Update views.py

--- authentication/views.py
+++ authentication/views.py
@@ -5,6 +5,7 @@ import json
 
 # Create your views here.
 def login_status(request):
+    print(request.body.decode('utf-8'))
     if not request.user.is_authenticated:
         response =  JsonResponse({'message': 'REDIRECT'}, status=501)
         return response
@@ -15,18 +16,22 @@ def login_status(request):
 def login_api(request):    
     req_body = request.body.decode('utf-8')
     req = json.loads(req_body)
+    print(req)
     email = req["email"]
     password = req["password"]
     user = authenticate(request, username=email, password=password)
-
-    if user is not None:
-        # Authentication successful
-        login(request, user)        
-        response =  JsonResponse({'message': 'SUCCESS'}, status=201)
-        return response   
-    else:
-        # Authentication failed
-        return JsonResponse({'message': 'FAILURE'}, status=401)
+    try:
+        if user is not None:
+            # Authentication successful
+            login(request, user)        
+            response =  JsonResponse({'message': 'SUCCESS'}, status=201)
+            return response   
+        else:
+            # Authentication failed
+            return JsonResponse({'message': 'FAILURE'}, status=401)
+    except Exception as e:
+        print("Error:",e)
+        return JsonResponse({'message': 'FAILURE'}, status=501)
     
 
 def logout_api(request):
