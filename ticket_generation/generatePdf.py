408c1e5656d546f8eb3d503ae1732bf3e8792ede
GB-MyLang <ghanashyam.b@mylang.in>
2023-11-01 09:55:11 +0530

    Revert "small change lets see if it works"
    This reverts commit b02218ea9fc9685609c6847c75f275e5e8979637.

--- ticket_generation/generatePdf.py
+++ ticket_generation/generatePdf.py
@@ -10,42 +10,55 @@ import os
 
 def generate_pdf(id,name,phone,qr_buffer):
     # Create a BytesIO buffer to store the PDF
-    
-    qr_pil_image = PILImage.open(qr_buffer)
-    image = Image.open("Rajyotsava.jpg")
-
-# Create a drawing context
-    draw = ImageDraw.Draw(image)
-
-    # Define the text, color, and size
-    text = "Dear "+ name+","
-    text_color = (253, 252, 220)  # RGB color (pale yellow)
-    text_size = 300
-
-
-    # Define the position where you want to add the text
-    text_position = (167, 740)
-    font = ImageFont.truetype("NotoSansKannada-Regular.ttf", 36)
-    # font = ImageFont.load_default()
-
-    # Draw the text on the image with the specified size
-    draw.text(text_position, text, fill=text_color, font=font)
-
-    image.paste(qr ,(545,1403))
-
     pdf_buffer = BytesIO()
-    c = canvas.Canvas(pdf_buffer, pagesize=letter)
 
-    # Convert the image to RGB mode
-    image = image.convert("RGB")
+    # Create the PDF document
+    pdf = canvas.Canvas(pdf_buffer, pagesize=letter)
 
-    # Save the image to the PDF
-    image.save(pdf_buffer, format="PDF")
+    # Add background color
+    pdf.setFillColorRGB(1, 1, 1)  # White
+    pdf.rect(0, 0, letter[0], letter[1], fill=True, stroke=False)
 
-    # Close the PDF canvas
-    c.save()
+    # Add border
+    pdf.setStrokeColor(colors.black)
+    pdf.rect(50, 50, letter[0] - 100, letter[1] - 100, fill=False, stroke=True)
 
-    # Reset the buffer to its beginning
+    # Add logo
+    logo_path = "static/logo.jpg"
+    pdf.drawImage(logo_path, 60, letter[1] - 160, width=100, height=100)
+
+    # Set up the certificate content
+    pdf.setFont("Helvetica-Bold", 24)
+    pdf.setFillColor(colors.black)
+    pdf.drawString(180, 500, "Kannada Rajyotsava")
+    pdf.setFont("Helvetica", 18)
+    pdf.drawString(180, 450, "Ticket Confirmation")
+    pdf.setFont("Helvetica-Bold", 20)
+    pdf.drawString(180, 400, name)
+    pdf.setFont("Helvetica", 18)
+    pdf.drawString(180, 350,id)
+    # pdf.setFont("Helvetica-Bold", 20)
+    # pdf.drawString(180, 300, event_name)
+
+    # Convert the QR code to a PIL image
+    qr_pil_image = PILImage.open(qr_buffer)
+
+    # Create a temporary PNG file to save the QR code
+    with tempfile.NamedTemporaryFile(suffix=".png", delete=False) as temp_qr_file:
+        temp_qr_file_path = temp_qr_file.name
+        qr_pil_image.save(temp_qr_file_path, format='PNG')
+
+    # Add the QR code image from the temporary file to the PDF
+    pdf.drawImage(temp_qr_file_path, 450, 55, width=1.5 * inch, height=1.5 * inch)
+
+    # Close and remove the temporary file
+    temp_qr_file.close()
+    os.remove(temp_qr_file_path)
+
+    # Save the PDF content to the buffer
+    pdf.save()
+
+    # Move the buffer position to the beginning of the PDF
     pdf_buffer.seek(0)
 
-    return pdf_buffer.getvalue()
+    return pdf_buffer
