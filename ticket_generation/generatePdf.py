a39dccd5fa47a8891a347bb0e97beca1f2c0fcef
GB-MyLang <ghanashyam.b@mylang.in>
2023-11-01 12:57:27 +0530

    Update generatePdf.py

--- ticket_generation/generatePdf.py
+++ ticket_generation/generatePdf.py
@@ -11,27 +11,6 @@ from io import BytesIO
 import PyPDF2
 import os
 
-import qrcode
-from io import BytesIO
-
-def generateQrCode(hash_val):
-    qr = qrcode.QRCode(
-            version=1,
-            error_correction=qrcode.constants.ERROR_CORRECT_H,
-            box_size=14,
-            border=1,
-        )
-
-    qr.add_data("KK-ATTENDEE_PASS-"+hash_val)
-    qr.make(fit=True)
-    qr_image = qr.make_image(fill_color="black", back_color="silver")
-
-    # Create a BytesIO buffer for the QR code image
-    qr_buffer = BytesIO()
-    qr_image.save(qr_buffer, format='PNG')  # Set the format explicitly to PNG
-    qr_buffer.seek(0)
-    return qr_buffer
-
 def generate_pdf(id,name,phone,qr_buffer):
     # Create a BytesIO buffer to store the PDF
     
@@ -81,7 +60,6 @@ def generate_pdf(id,name,phone,qr_buffer):
     finalPdf.addPage(pdf_buffer)
     finalPdf.addPage(qr_buffer)
     finalPdf.write(finalpdf_buffer)
+    finalpdf_buffer.seek(0)
 
     return finalpdf_buffer
-
-generate_pdf("ID","NAME","PHONE",qr_buffer=generateQrCode("HASH"))
