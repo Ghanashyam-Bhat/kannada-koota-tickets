498891194c9ed6b0a123ecee26443fbf0611ac12
GB-MyLang <ghanashyam.b@mylang.in>
2023-11-01 19:57:06 +0530

    clear migrations

--- ticket_generation/models.py
+++ ticket_generation/models.py
@@ -1,11 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 import datetime
-from datetime import timedelta
+import pytz
 # Create your models here.
-
-def current_time():
-    return (datetime.utcnow() + timedelta(hours=5, minutes=30)).isoformat()
 
 class attendee(models.Model):
     id = models.CharField(max_length=20,primary_key=True)
@@ -16,4 +13,15 @@ class attendee(models.Model):
     handledBy = models.ForeignKey(User,on_delete=models.SET_NULL,null=True)
     hashVal = models.CharField(max_length=64)
     isVip = models.BooleanField(default=False)
-    created_at = models.DateTimeField(default=current_time)
+    created_at = models.DateTimeField(default=(datetime.datetime.now().replace(tzinfo=pytz.utc).astimezone(pytz.timezone('Asia/Kolkata'))).isoformat(' '))
+
+
+
+# Get the current date and time
+current_datetime = datetime.datetime.now()
+
+# Define a timedelta for 5 hours and 30 minutes
+time_delta = datetime.timedelta(hours=5, minutes=30)
+
+# Add the timedelta to the current datetime
+new_datetime = current_datetime + time_delta
