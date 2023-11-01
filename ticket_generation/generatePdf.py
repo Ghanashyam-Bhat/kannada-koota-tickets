0d5aef3718fc9c116101442e8b8eebd29d3fa170
GB-MyLang <ghanashyam.b@mylang.in>
2023-11-01 09:55:07 +0530

    Revert "ye kaam kar jaye to thik hai"
    This reverts commit 2478f5e6572fd4f881223e8223189718969630c4.

--- ticket_generation/generatePdf.py
+++ ticket_generation/generatePdf.py
@@ -9,51 +9,43 @@ from io import BytesIO
 import os
 
 def generate_pdf(id,name,phone,qr_buffer):
+    # Create a BytesIO buffer to store the PDF
+    
     qr_pil_image = PILImage.open(qr_buffer)
+    image = Image.open("Rajyotsava.jpg")
 
-    # Create a temporary PNG file to save the QR code
-    with tempfile.NamedTemporaryFile(suffix=".png", delete=False) as temp_qr_file:
-        temp_qr_file_path = temp_qr_file.name
-        qr_pil_image.save(temp_qr_file_path, format='PNG')
-
-    image = PILImage.open("Rajyotsava.jpg")
-
-    # Create a drawing context
+# Create a drawing context
     draw = ImageDraw.Draw(image)
 
     # Define the text, color, and size
-    text = "Dear " + name + ","
+    text = "Dear "+ name+","
     text_color = (253, 252, 220)  # RGB color (pale yellow)
     text_size = 300
+
 
     # Define the position where you want to add the text
     text_position = (167, 740)
     font = ImageFont.truetype("NotoSansKannada-Regular.ttf", 36)
+    # font = ImageFont.load_default()
 
     # Draw the text on the image with the specified size
     draw.text(text_position, text, fill=text_color, font=font)
 
-    # Open the QR code image from the temporary file
-    qr_code_img = PILImage.open(temp_qr_file_path)
+    image.paste(qr ,(545,1403))
 
-    # Paste the QR code image onto the existing image
-    image.paste(qr_code_img, (545, 1403))
-
-    # Create a PDF buffer
     pdf_buffer = BytesIO()
+    c = canvas.Canvas(pdf_buffer, pagesize=letter)
 
-    # Create the PDF document
-    pdf = canvas.Canvas(pdf_buffer, pagesize=letter)
+    # Convert the image to RGB mode
+    image = image.convert("RGB")
 
-    # Define the size to fit the full page
-    image_size = (letter[0], letter[1])
+    # Save the image to the PDF
+    image.save(pdf_buffer, format="PDF")
 
-    # Use ImageReader to read the PIL image and insert it into the PDF
-    pdf.drawImage(ImageReader(image), 0, 0, width=image_size[0], height=image_size[1])
+    # Close the PDF canvas
+    c.save()
 
-    pdf.save()
-
-    # Move the buffer position to the beginning of the PDF
+    # Reset the buffer to its beginning
     pdf_buffer.seek(0)
 
-    return pdf_buffer
+    return pdf_buffer.getvalue()
