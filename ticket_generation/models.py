f5c747fac6cf4b0d71b76a28813bffb06a310f72
GB-MyLang <ghanashyam.b@mylang.in>
2023-11-03 10:38:17 +0530

    migrate

--- ticket_generation/models.py
+++ ticket_generation/models.py
@@ -1,14 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import User
+
 # Create your models here.
 
+
 class attendee(models.Model):
-    id = models.CharField(max_length=20,primary_key=True)
+    id = models.CharField(max_length=100, primary_key=True)
     email = models.EmailField()
-    name = models.CharField(max_length=50)
-    phone = models.CharField(max_length=10)
+    name = models.CharField(max_length=500)
+    phone = models.CharField(max_length=100)
     isCash = models.BooleanField()
-    handledBy = models.ForeignKey(User,on_delete=models.SET_NULL,null=True)
+    handledBy = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     hashVal = models.CharField(max_length=64)
     isVip = models.BooleanField(default=False)
     created_datetime = models.DateTimeField()
