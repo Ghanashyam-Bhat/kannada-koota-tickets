448849cb07ea711d4da86e299ff84aa3a55dbfe3
Ghanashyam Bhat <74894094+Ghanashyam-Bhat@users.noreply.github.com>
2023-11-03 09:49:23 +0530

    Update generatePdf.py

--- ticket_generation/generatePdf.py
+++ ticket_generation/generatePdf.py
@@ -10,28 +10,7 @@ import tempfile
 from io import BytesIO
 import os
 
-import qrcode
-from io import BytesIO
-
-def generateQrCode(hash_val):
-    qr = qrcode.QRCode(
-            version=1,
-            error_correction=qrcode.constants.ERROR_CORRECT_H,
-            box_size=14,
-            border=1,
-        )
-
-    qr.add_data("KK-ATTENDEE_PASS-"+hash_val)
-    qr.make(fit=True)
-    qr_image = qr.make_image(fill_color="black", back_color="silver")
-
-    # Create a BytesIO buffer for the QR code image
-    qr_buffer = BytesIO()
-    qr_image.save(qr_buffer, format='PNG')  # Set the format explicitly to PNG
-    qr_buffer.seek(0)
-    return qr_buffer
-
-def generate_pdf(id,name,contact,qrCode,isVip):
+def generate_pdf(id,name,phone,qr_buffer,isVip)::
     # Create a BytesIO buffer to store the PDF
 
     # pdf = canvas.Canvas("certificate.pdf", pagesize=letter)
@@ -76,9 +55,5 @@ def generate_pdf(id,name,contact,qrCode,isVip):
 
     # Move the buffer position to the beginning of the PDF
     pdf_buffer.seek(0)
-    # Assuming pdf_buffer contains the PDF content
-    # with open('output.pdf', 'wb') as output_file:
-    #     output_file.write(pdf_buffer.read())
 
     return pdf_buffer
-# generate_pdf("hitesh",True)
